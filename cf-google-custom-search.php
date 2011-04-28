82eca7e826a19923888b2f128204c6e26da084d1
smathias <smathias@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-04-28 20:10:47 +0000

    Fixed version number
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-google-custom-search/trunk@36016 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-google-custom-search.php
+++ cf-google-custom-search.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Google Custom Search
 Plugin URI:
 Description: Utilize Google's Custom Search API instead of WordPress' search functionality.
-Version: 1.0
+Version: 1.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com/
 */
